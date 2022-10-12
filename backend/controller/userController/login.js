e9631863e45ea1618fa580a866ae87a5870182c4
Kelvide <68742854+Kelvide@users.noreply.github.com>
2022-10-12 16:45:11 +0100

    Nodemailer working after registration

--- backend/controller/userController/login.js
+++ backend/controller/userController/login.js
@@ -1,6 +1,15 @@
 const admin = require("../../db/firebase");
+const nodemailer = require("nodemailer");
+require('dotenv').config()
 
 const signup = async (req,res)=>{
+  const transporter = nodemailer.createTransport({
+    service:"hotmail",
+    auth: {
+      user:process.env.EMAIL,
+      pass:process.env.PASS
+    }
+  })
   let { email, password }=req.body;
     admin.auth().createUser({email, password,emailVerified: false})
     .then((userCredential) => {
@@ -8,7 +17,19 @@ const signup = async (req,res)=>{
       admin.auth().generateEmailVerificationLink(user.email)
       .then((link) => {
         admin.firestore().collection('users').doc().set({uid:user.uid, balance:0}).then(()=>{
-          res.send({user, link})
+          const options={
+            from:process.env.EMAIL,
+            to:user.email,
+            subject: "Email Verification From Banka App",
+            text:  `Your Email Verification link is ${link}`
+          }
+          transporter.sendMail(options,function(err,info){
+            if (err){
+              res.send(err.message)
+              return;
+            }
+            res.send({user, info:info.response})
+          })
         }).catch(err=>{
           res.send(err.message)
         })
