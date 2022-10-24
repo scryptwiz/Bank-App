7b72414d348b2e57f3902f1bb59d5de218264838
Kelvide <68742854+Kelvide@users.noreply.github.com>
2022-10-24 22:48:07 +0100

    added a mailer after signing up

--- backend/controller/userController/profileController.js
+++ backend/controller/userController/profileController.js
@@ -3,6 +3,7 @@ const usersModel = require("../../models/usersModel");
 require('dotenv').config()
 
 const signup = async (req,res)=>{
+  let { username, email, password }=req.body;
   const transporter = nodemailer.createTransport({
     service:"hotmail",
     auth: {
@@ -10,31 +11,36 @@ const signup = async (req,res)=>{
       pass:process.env.PASS
     }
   })
-  let { username, email, password }=req.body;
+  let mailOptions = {
+    from: '"Banka App"',
+    to: `${email}`,
+    subject: "Banka App Account Verification ✔",
+    text: "Here is your verification link",
+    html: "<a href='http://localhost:2600/api/user/verify/'>Verify Your Email</a>",
+  };
   const account_no = Math.floor(10000000000 + Math.random() * 90000000000)
   if (email&&password&&account_no) {
     const signup = new usersModel({username, email, account_no, password})
     signup.save(err=>{
       if(!err) {
         try {
-          let info =  transporter.sendMail({
-            from: '"Banka App"',
-            to: `${email}`,
-            subject: "Banka App Account Verification ✔",
-            text: "Here is your verification link",
-            html: "<a href='http://localhost:2600/api/user/verify/'>Verify Your Email</a>",
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              res.json({message:"Signed Up Successfully make sure you verify your email", status: true});
+            } else {
+              res.json({message:"Signed Up Successfully", info:info.response,status: true});
+            }
           });
-          res.json({message:"Signed Up Successfully", info:info,status: true});
         } catch(err){
           res.json({message:err.message, status: false});
         }
     } else if (err) {
-        if (err) {
-            res.json({message:err.message, status: false})
+        if (err.keyPattern.email==1) {
+            res.json({message:"Email Already Existed", status: false})
         } else if (err.keyPattern.username==1) {
           res.json({message:"Username Already Existed", status: false})
-        } else  if (err.keyPattern.email==1){
-            res.json({message:"Email Already Existed", status:false})
+        } else {
+            res.json({message:err.message, status:false})
         }
     }
     })
