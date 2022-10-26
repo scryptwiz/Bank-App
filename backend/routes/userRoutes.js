fa6037ac377a894f1e2d79b09c8ee9e7aba0fd92
Kelvide <68742854+Kelvide@users.noreply.github.com>
2022-10-26 22:13:29 +0100

    added  the signin function

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signup, updateProfile, verifyUser } = require('../controller/userController/profileController');
+const { signup, updateProfile, verifyUser, signin } = require('../controller/userController/profileController');
 // const { fetchTransaction, transferMoney } = require('../controller/userController/transactionController');
 const routes = express.Router()
 
@@ -7,6 +7,7 @@ const routes = express.Router()
 routes.post('/user/signup', signup)
 routes.get('/user/verifyUser/:id', verifyUser)
 routes.post('/user/updateProfile', updateProfile)
+routes.post('/user/signin', signin)
 
 // user transaction routes
 // routes.post('/user/fetchTransaction', fetchTransaction)
