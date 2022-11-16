144311aba90848bc9378bb9a3c411a6c158345a6
Kelvin Ajayi <89777325+Marvel-Deone@users.noreply.github.com>
2022-11-16 08:27:58 +0000

    Worked on the connection

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,13 +1,13 @@
+import axios from 'axios';
+import { useFormik } from 'formik';
+import { useSnackbar } from "notistack";
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useSnackbar } from "notistack";
-import { useFormik } from 'formik';
-import axios from 'axios';
 import * as Yup from 'yup';
 
 export const Signup = () => {
   const { enqueueSnackbar } = useSnackbar()
-// const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const initialValues = {
     username: '',
@@ -17,11 +17,11 @@ export const Signup = () => {
   }
   const onSubmit = values => {
     console.log(values);
-    axios.post('http://localhost:2600/api/user/signup', values).then(res=> {
+    axios.post('http://localhost:2600/api/user/signup', values).then(res => {
       if (res.data.status) {
         console.log('res', res, 'resData', res.data);
         enqueueSnackbar(`${res.data.message}`, { variant: "success" });
-      }else {
+      } else {
         console.log('error');
         enqueueSnackbar(`${res.data.message}`, { variant: "error" });
       }
@@ -36,7 +36,7 @@ export const Signup = () => {
     password: Yup.string().matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/, "Password must contain at least 1 uppercase, 1 lower case, 1 number, 1 special character and must be up to or more than 8").required("Field is required"),
     confirmpassword: Yup.string().oneOf([Yup.ref('password'), null], 'Password must match').required('Field is required')
   })
-  const formik  = useFormik({
+  const formik = useFormik({
     initialValues,
     onSubmit,
     validationSchema
@@ -55,7 +55,7 @@ export const Signup = () => {
                 alt="Office"
               />
             </div>
-            <div className="flex items-center justify-center p-6 sm:p-12 md:w-3/5">
+            <div className="flex flex-col p-6 sm:p-12 md:w-3/5">
               <div className="w-full">
                 <h1
                   className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200"
@@ -69,7 +69,7 @@ export const Signup = () => {
                       className="block w-full mt-1 text-sm border px-3 py-2 rounded-lg border-purple-200 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple text-gray-500"
                       placeholder="Jane Doe" name='username' onChange={formik.handleChange} value={formik.values.username}
                     />
-                    { formik.touched.username && formik.errors.username && <small className='text-red-500'>{formik.errors.username}</small> }
+                    {formik.touched.username && formik.errors.username && <small className='text-red-500'>{formik.errors.username}</small>}
                   </div>
                   <div>
                     <label className="text-gray-700 text-sm font-semibold">Email</label>
@@ -77,7 +77,7 @@ export const Signup = () => {
                       className="block w-full mt-1 text-sm border px-3 py-2 rounded-lg border-purple-200 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple text-gray-500"
                       placeholder="Jane Doe" name='email' onChange={formik.handleChange} value={formik.values.email}
                     />
-                    { formik.touched.email && formik.errors.email && <small className='text-red-500'>{formik.errors.email}</small> }
+                    {formik.touched.email && formik.errors.email && <small className='text-red-500'>{formik.errors.email}</small>}
                   </div>
                   <div>
                     <label className="text-gray-700 text-sm font-semibold">Password</label>
@@ -86,7 +86,7 @@ export const Signup = () => {
                       placeholder="***************"
                       type="password" name='password' onChange={formik.handleChange} value={formik.values.password}
                     />
-                    { formik.touched.password && formik.errors.password && <small className='text-red-500'>{formik.errors.password}</small> }
+                    {formik.touched.password && formik.errors.password && <small className='text-red-500'>{formik.errors.password}</small>}
                   </div>
                   <div>
                     <label className="text-gray-700 text-sm font-semibold">Confirm Password</label>
@@ -95,7 +95,7 @@ export const Signup = () => {
                       placeholder="***************"
                       type="password" name='confirmpassword' onChange={formik.handleChange} value={formik.values.confirmpassword}
                     />
-                    { formik.touched.confirmpassword && formik.errors.confirmpassword && <small className='text-red-500'>{formik.errors.confirmpassword}</small> }
+                    {formik.touched.confirmpassword && formik.errors.confirmpassword && <small className='text-red-500'>{formik.errors.confirmpassword}</small>}
                   </div>
                   <div className="flex mt-6 text-sm">
                     <label className="flex items-center dark:text-gray-400">
@@ -109,11 +109,11 @@ export const Signup = () => {
                       </span>
                     </label>
                   </div>
-                <button type='submit'
-                  className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-                >
-                  Create account
-                </button>
+                  <button type='submit'
+                    className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                  >
+                    Create account
+                  </button>
                 </form>
 
                 <hr className="mt-6 mb-3" />
